a0ef9adc1b93c6bf38b0f9bfbc53b49849d2c398
long <tranbaolong2018tbb@gmail.com>
2022-12-04 22:50:20 +0700

    update-image-content

--- public/javascript/img-gallery.js
+++ public/javascript/img-gallery.js
@@ -4,3 +4,21 @@ function myFunction(imgs) {
     expandImg.parentElement.style.display = "block";
   }
 
+function myFunction1(imgs1) {
+  var expandImg1 = document.getElementById("expandedImg1");
+    expandImg1.src = imgs1.src;
+    expandImg1.parentElement.style.display = "block";
+  }
+
+function myFunction2(imgs2) {
+  var expandImg2 = document.getElementById("expandedImg2");
+    expandImg2.src = imgs2.src;
+    expandImg2.parentElement.style.display = "block";
+}
+
+function myFunctionTC(imgsTC) {
+  var expandImgTC = document.getElementById("expandedImgTC");
+    expandImgTC.src = imgsTC.src;
+    expandImgTC.parentElement.style.display = "block";
+  }
+  
