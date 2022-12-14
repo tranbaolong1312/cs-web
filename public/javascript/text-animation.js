d2e6025ea770812f6864d68eaf423a1aabad49d5
long <tranbaolong2018tbb@gmail.com>
2022-12-15 01:11:48 +0700

    update_animation_15-12

---
+++ public/javascript/text-animation.js
@@ -0,0 +1,62 @@
+// window.onbeforeunload = function () {
+//     window.scrollTo(0, 0);
+//   }
+window.onload = function() {
+    //SECTION2
+    document.getElementById('text1104').classList.add("fade-animation-top");
+    document.getElementById('box1097').classList.add("fade-animation-top");
+    document.getElementById('text1096').classList.add("fade-animation-top");
+    document.getElementById('list1103').classList.add("fade-animation-left");
+    document.getElementById('text1106').classList.add("fade-animation-left");
+    document.getElementById('head7').classList.add("fade-animation-left");
+    document.getElementById('BUTTON1108').classList.add("fade-animation-left");
+    document.getElementById('IMAGE1429').classList.add("fade-animation-right");
+
+    //SECTION6
+    document.getElementById('head1111').classList.add("fade-animation-top");
+    document.getElementById('head1112').classList.add("fade-animation-top");
+    document.getElementById('LIST_PARAGRAPH1780new').classList.add("fade-animation-left");
+    document.getElementById('GALLERY1113').classList.add("fade-animation-right");
+    document.getElementById('BUTTON1114').classList.add("fade-animation-left");
+
+    //SECTION1118
+    document.getElementById('BOX1127').classList.add("fade-animation-top");
+    document.getElementById('HEADLINE1120').classList.add("fade-animation-top");
+    document.getElementById('HEADLINE1126').classList.add("fade-animation-top");
+    document.getElementById('GALLERY1122').classList.add("fade-animation-left");    
+    document.getElementById('LIST_PARAGRAPH1121').classList.add("fade-animation-right");
+
+    //SECTION1774
+    document.getElementById('HEADLINE1781').classList.add("fade-animation-top");
+    document.getElementById('BOX1788').classList.add("fade-animation-left");
+    document.getElementById('LIST_PARAGRAPH1780').classList.add("fade-animation-left");
+    document.getElementById('HEADLINE1787').classList.add("fade-animation-left");
+    document.getElementById('GALLERY1114').classList.add("fade-animation-right");
+    document.getElementById('BUTTON1855').classList.add("fade-animation-left");
+    document.getElementById('GROUP1794').classList.add("fade-animation-right");
+    document.getElementById('GALLERY1115').classList.add("fade-animation-left");
+    document.getElementById('BUTTON1857').classList.add("fade-animation-right");
+    document.getElementById('BOX1796').classList.add("fade-animation-left");
+    document.getElementById('LIST_PARAGRAPH1797').classList.add("fade-animation-left");
+    document.getElementById('BUTTON1859').classList.add("fade-animation-left");
+    
+    //SECTION1440
+    document.getElementById('HEADLINE1197').classList.add("fade-animation-top");
+    document.getElementById('GALLERY1452').classList.add("fade-animation-right");
+    document.getElementById('LIST_PARAGRAPH1202').classList.add("fade-animation-left");
+    document.getElementById('BUTTON1453').classList.add("fade-animation-left");
+
+    //SECTION1455
+    document.getElementById('HEADLINE1458').classList.add("fade-animation-top");
+    document.getElementById('IMAGE1862').classList.add("fade-animation-right");
+    document.getElementById('LIST_PARAGRAPH1463').classList.add("fade-animation-left");
+    document.getElementById('BUTTON1465').classList.add("fade-animation-left");
+
+    //SECTION1812
+    document.getElementById('BOX1813').classList.add("fade-animation-top");
+    document.getElementById('HEADLINE1814').classList.add("fade-animation-top");
+    document.getElementById('HEADLINE1827').classList.add("fade-animation-top");
+    document.getElementById('IMAGE1830').classList.add("fade-animation-right");
+    document.getElementById('GROUP1831').classList.add("fade-animation-left");    
+    document.getElementById('HEADLINE1817').classList.add("fade-animation-left");
+  };
