03b936fb45411dd5a23c725b215bd62682f22f14
KuroShinigami318 <53936920+KuroShinigami318@users.noreply.github.com>
2022-12-17 10:17:15 +0000

    Add Transition Image Galery Service

--- public/javascript/img-gallery.js
+++ public/javascript/img-gallery.js
@@ -22,3 +22,23 @@ function myFunctionTC(imgsTC) {
     expandImgTC.parentElement.style.display = "block";
   }
   
+function loopService() {
+  const galaryList = ["expandedImg", "expandedImg1", "expandedImg2", "expandedImgTC"];
+  galaryList.transitionImg = (index) => {
+    let expandImg = document.getElementById(this[index]);
+    const Imgs = expandImg.parentElement.nextElementSibling.querySelectorAll("img");
+    const interval = (i_expandImg, i_Imgs, i) => {
+      i_expandImg.src = i_Imgs[i].src;
+      i_expandImg.parentElement.style.display = "block";
+      if (i < Imgs.length()) ++i;
+      else i = 0;
+      setTimeout(interval, 1000, i_expandImg, i_Imgs, i);
+    };
+    interval(0, expandImg, Imgs, 0);
+  };
+
+  for(let i = 0; i < galaryList; ++i)
+  {
+    galaryList.transitionImg(i);
+  }
+}
