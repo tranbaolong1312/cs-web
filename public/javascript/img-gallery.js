ca234a424284904d74b56bc01a293cf8947bee69
long <tranbaolong2018tbb@gmail.com>
2023-01-07 23:21:57 +0700

    uplive 4.2

--- public/javascript/img-gallery.js
+++ public/javascript/img-gallery.js
@@ -5,7 +5,7 @@ function ImageReplace(i_expandImg, i_Img) {
 }
   
 function loopService() {
-  // Add or Update ID here
+  // Add or Update ID imgs here
   const galleryList = ["expandedImg", "expandedImg1", "expandedImg2", "expandedImg3", "expandedImg4", "expandedImg5", "expandedImg6", "expandedImg7", "expandedImg8", "expandedImg9", "expandedImg10", "expandedImg11"];
   galleryList.transitionImg = function(index) {
     let expandImg = document.getElementById(this[index]);
@@ -40,4 +40,21 @@ function loopService() {
   {
     galleryList.transitionImg(i);
   }
+  //Add or Update ID BUTTON 
+  const buttonList = ["BUTTON1108","BUTTON1114","BUTTON1115","BUTTON1116","BUTTON1117","BUTTON1118","BUTTON1119"];
+  buttonList.run = function(index) {
+    let button = document.getElementById(this[index]);
+    button.onclick = function() {
+      document.getElementById("POPUP1799").style.display = "block";
+      document.getElementById("form-animation").classList.add("fade-animation-form");
+    };
+  }
+    for(let i = 0; i < buttonList.length; ++i)
+    {
+      buttonList.run(i);
+    };
+  let divclose = document.getElementsByClassName("popup-close")[0];
+  divclose.onclick = function() {
+    document.getElementById("POPUP1799").style.removeProperty("display");
+  };
 }
